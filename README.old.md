88256e508f27927c7b00ce9e92fbed9940d1b22d
Đức Tân <85245950+thaiductan@users.noreply.github.com>
2022-06-21 17:28:59 +0700

    Update Setup Environment v1
    setup thêm các file môi trường và code header

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# Food_Store_Dana
+Fullstack web bán các loại thức ăn ở đà nẵng
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Header } from "./components";
+
+const app = () => {
+  return (
+    <div className="w-screen h-screen flex flex-col">
+      <Header />
+    </div>
+  );
+};
+
+export default app
